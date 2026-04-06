f9f594fa5fbf6e98d1553251ddbe389bed73515d
themdshakibul <mdshakibul88240@gmail.com>
2026-04-06 07:07:14 +0600

    39-3 Book data Width api

--- src/Components/HomePage/Heder.jsx
+++ src/Components/HomePage/Heder.jsx
@@ -2,12 +2,14 @@ import BooksImages from "../../assets/pngwing1.png";
 const Heder = () => {
   return (
     <frames>
-      <section className="container mx-auto px-2 mt-5 py-30 bg-base-300 rounded-2xl md:flex items-center justify-center gap-10 lg:gap-80">
+      <section className="container mx-auto px-2 mt-5 py-30 bg-base-300 rounded-2xl flex flex-col md:flex-row items-center justify-center gap-10 lg:gap-70 mb-10 md:mb-20 lg:mb-30">
         <div>
           <h2 className="text-7xl font-bold">
             Books to freshen up <br /> your bookshelf
           </h2>
-          <button className="btn btn-success font-bold mt-10">View The List</button>
+          <button className="btn btn-success font-bold mt-10">
+            View The List
+          </button>
         </div>
         <img src={BooksImages} alt="" />
       </section>
--- src/Pages/HomePage/HomePage.jsx
+++ src/Pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Heder from "../../Components/HomePage/Heder";
+import AllBooks from "../../Components/HomePage/AllBooks";
 
 function HomePage() {
   return (
     <frames>
       <Heder />
+      <AllBooks />
     </frames>
   );
 }
